2ef535e143f86f61cb7e91a9d7a0b0f901b09e18
Aditya Kumar <adityaara7667@gmail.com>
2026-04-14 19:20:10 +0530

    add morre

---
+++ stack/push.c
@@ -0,0 +1,69 @@
+
+// This Code not print element because not we have display functio. only store element or data
+// ------------------------------------------------------------------------------------------------------------
+
+
+// #include <stdio.h>
+// #define MAX_SIZE 10  // we can change variable name and size
+// int stack[MAX_SIZE];
+// int top = -1;
+// void push(int item)
+// {
+//     if (top == MAX_SIZE - 1)
+//     {
+//         printf("Overflow & Exit");
+//     }
+//     else
+//     {
+//         stack[++top] = item;
+//         printf("%d item inserted . \n", item);
+//     }
+// }
+// int main()
+// {
+//     int item, n;
+//     printf("Enter howmany element you want to add:");
+//     scanf("%d", &n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("Enter element:");
+//         scanf("%d", &item);
+//         push(item);
+//     }
+
+//     return 0;
+// }
+
+
+// -----------------------------------------------------------------------------------------------------------
+// Depents on user so that how many add items
+// _______________________________________________________________________________________________________________
+
+// #include<stdio.h>
+// #define max_size 5 // we can change variable name and size
+// int my_stack[max_size];
+// int top = -1;
+// void push (int item){
+//     if (top == max_size-1)
+//     {
+//         printf("Overflow");
+//     }
+//     else{
+//         my_stack[++top] = item ;
+//         printf("%d item inserted",item);
+//     }
+// }
+// int main (){
+//     int item ,n;
+//     do
+//     {
+//         printf("Enter element:");
+//         scanf("%d",&item);
+//         push(item);
+//         printf("Do you add more items (yes 1 ,No 0)");
+//         scanf("%d",&n);
+//     } while (n == 1);
+//     return 0;
+    
+// }
+
